34b96e58acfc3b4d0a70569739c9545f1a0ea184
Dmitry Skrebkov <skredmi91@gmail.com>
2023-03-14 20:43:27 +0300

    fixed path route

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
     <Router>
       <div className={styles.app}>
         <Routes>
-          <Route exact path="/" element={<Main />} />
-          <Route path="/order" element={<Message />} />
+          <Route exact path="/fakestore-rtk" element={<Main />} />
+          <Route path="/fakestore-rtk/order" element={<Message />} />
         </Routes>
       </div>
     </Router>
